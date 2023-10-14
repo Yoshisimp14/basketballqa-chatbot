9734102985b590322153685801a3963541db6fb2
Yoshisimp14 <147840250+Yoshisimp14@users.noreply.github.com>
2023-10-14 11:05:48 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,79 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import time
+import random
 
-LOGGER = get_logger(__name__)
+def get_most_similar_response(df, query, top_k=1):
+    vectorizer = TfidfVectorizer()
+    all_data = list(df['Question']) + [query]
+    tfidf_matrix = vectorizer.fit_transform(all_data)
+    document_vectors = tfidf_matrix[:-1]
+    query_vector = tfidf_matrix[-1]
+    similarity_scores = cosine_similarity(query_vector, document_vectors)
+    sorted_indexes = similarity_scores.argsort()[0][::-1][:top_k]
+    most_similar_responses = df.iloc[sorted_indexes]
+    return most_similar_responses
 
+# Read the data from the CSV file
+df = pd.read_csv('basketball_qa.csv')
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+st.title("Basketball Q&A")
 
-    st.write("# Welcome to Streamlit! 👋")
+description = """
+This chatbot is your ultimate companion for exploring the exciting world of "Basketball". It is a high-paced team sport played on a rectangular court. Teams aim to score points by shooting a ball through the opponent's hoop. It's known for its dynamic gameplay, iconic players, and global appeal. Basketball promotes fitness, teamwork, and community. Let's embrace ourselves on knowning more about basketball with the help of the chatbot
+"""
 
-    st.sidebar.success("Select a demo above.")
+st.markdown(description)
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+# Initialize empty lists to store chatbot responses and questions
+chatbot_responses = []
+chatbot_questions = []
 
+# Initialize a variable to track the last user interaction time
+last_interaction_time = time.time()
 
-if __name__ == "__main__":
-    run()
+while True:
+    current_time = time.time()
+    # Check if it has been at least 10 seconds since the last user interaction
+    if current_time - last_interaction_time >= 10:
+        # Randomly select a question from the dataset
+        random_index = random.randint(0, len(df) - 1)
+        random_question = df.iloc[random_index]['Question']
+
+        chatbot_questions.append(random_question)
+        chatbot_responses.append("I'm here to help with any basketball-related questions. Feel free to ask!")
+
+        print(f"Chatbot: {random_question}")
+        last_interaction_time = current_time
+
+    query = input("You: ")
+    last_interaction_time = time.time()  # Update the last interaction time with user input
+
+    if query.lower() == 'exit':
+        # If the user types 'exit,' end the conversation
+        chatbot_questions.append("exit")
+        chatbot_responses.append("Chatbot: Goodbye!")
+
+        # Create a DataFrame for chatbot responses and questions
+        chatbot_df = pd.DataFrame({
+            'Question': chatbot_questions,
+            'Answer': chatbot_responses
+        })
+
+        # Save the chatbot responses to a CSV file
+        chatbot_df.to_csv('chatbot_responses.csv', index=False)
+
+        print("Chatbot: Goodbye!")
+        break
+
+    response_df = get_most_similar_response(df, query)
+    if not response_df.empty:
+        response = response_df.iloc[0]['Answer']
+        question = response_df.iloc[0]['Question']
+
+        chatbot_questions.append(question)
+        chatbot_responses.append(response)
+
+        print(f"Chatbot: {response}")
